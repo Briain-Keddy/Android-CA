747755b29b1fd9ded7533974475afe8c510caa55
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2022-01-04 11:15:00 +0000

    First Commit

--- app/src/main/java/com/example/ca_1/MainViewModel.kt
+++ app/src/main/java/com/example/ca_1/MainViewModel.kt
@@ -2,14 +2,32 @@ package com.example.ca_1
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.ca_1.data
-import com.example.ca_1
+import androidx.lifecycle.viewModelScope
+import com.example.ca_1.data.TvShowEntity
+import com.example.ca_1.dataaccess.RetrofitInstance
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    val tvShowList = MutableLiveData<List<TvShowEntity>>()
+
+    private val _tvShowList:  MutableLiveData<List<TvShowEntity>>() = MutableLiveData()
+
+    val tvShows: LiveData<List<TvShowEntity>>
+    get() = _tvShowList
+
+    private val _isLoading = MutavlesLiveData(false)
+    val isLoading: LiveData<Boolean>
+    get() = _isLoading
 
     init {
-        tvShowList.value = SampleDataProvider.getTvShow()
+        getTvShow()
+    }
+    private fun getTvShow(){
+        viewModelScope.launch{
+            _isLoading.value = true
+            val fetchedTvShow = RetrofitInstance.api.getTvShow()
+            Log.i(TAG, "List of TV Shows : $fetchedTvShow")
+            _tvShows.value = fetchedTvShow
+            _isLoading.value = false
+        }
     }
 }
